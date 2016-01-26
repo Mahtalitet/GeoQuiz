a3c3b3ea1aba60ce51d8ce2a1c7b390a4dd52f17
ilya <MAAFI4I@gmail.com>
2016-01-26 18:05:30 +0200

    Before adding Listeners

--- src/com/example/geoquiz/QuizActivity.java
+++ src/com/example/geoquiz/QuizActivity.java
@@ -1,16 +1,22 @@
 package com.example.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class QuizActivity extends ActionBarActivity {
 
+	private Button mTrueButton;
+	private Button mFalseButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+		mTrueButton = (Button) findViewById(R.id.true_button);
+		mFalseButton = (Button) findViewById(R.id.false_button);
 	}
 
 	@Override
