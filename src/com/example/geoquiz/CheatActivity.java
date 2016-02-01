9fa64fe06e0af3d99cd201487cb598a55c9bde57
ilya <MAAFI4I@gmail.com>
2016-02-01 19:58:55 +0200

    After adding a new activity CheatActivity for new layout

---
+++ src/com/example/geoquiz/CheatActivity.java
@@ -0,0 +1,23 @@
+package com.example.geoquiz;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class CheatActivity extends ActionBarActivity {
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+		
+		
+		
+		
+	}
+	
+}
