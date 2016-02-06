8cb8cf30a41a436000bfcb53229a393ebd6224af
ilya <MAAFI4I@gmail.com>
2016-02-06 18:07:39 +0200

    After resolving the problem with ActionBar, v1

--- src/com/example/geoquiz/QuizActivity.java
+++ src/com/example/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -48,6 +49,12 @@ public class QuizActivity extends ActionBarActivity {
 		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
 		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			ActionBar actionBar = getSupportActionBar();
+			actionBar.setSubtitle("xaxaxa");
+			
+		}
+
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
 			@Override
